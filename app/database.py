805a4647c1a6c3106b850972c506806b4d173fda
Aleksandr <alexbardysov96@gmail.com>
2023-09-02 07:48:33 +0000

    add dependency injections

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+import json
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import insert
 from models import engine_db
@@ -7,9 +8,12 @@ from log import logger
 
 class Operations:
     """class with database operations"""
+    
+    def __init__(self) -> None:
+        self.connector = engine_db
 
-    def add_company(data):
-        with engine_db.connect() as session:
+    def add_company(self, data: json) -> None:
+        with self.connector.connect() as session:
             try:
                 statement = insert(Company).values(**data)
                 session.execute(statement)
--- app/main.py
+++ app/main.py
@@ -6,76 +6,85 @@ from database import Operations
 from settings import settings
 
 
-def archive_unpacker(part_length):
-    """unpacks the archive and alternately gives the contents of the files to json"""
+class Main:
+    """the main class of project implementing the bussiness logic"""
 
-    with ZipFile(f"../{settings.ARCHIVE}", mode="r") as archive:
-        filenames = [filename for filename in archive.namelist()]
-
-        partition = (filenames[i:i + part_length] for i in range(0, len(filenames), part_length))
-
-        for part in list(partition):
-            archive.extractall(members=part)
-            for filename in part:
-                with open(filename, encoding='utf-8') as file_json:
-                    logger.info(f"file {filename} unpacked and open")
-
-                    file_data = json.load(file_json)
-                    yield file_data
-
-                os.remove(filename)
-                logger.info(f"file {filename} removed")
-
-
-def address_handler(addr):
-    """converts address data to a string"""
-
-    if addr.get('Корпус'):
-        subaddr = ', ' + addr['Корпус']
-    elif addr.get('Кварт'):
-        subaddr = ', ' + addr['Кварт']
-    else:
-        subaddr = ''
-
-    result = f"""{addr['Регион']['НаимРегион']} {addr['Регион']['ТипРегион']}, 
-        {addr['Город']['ТипГород']} {addr['Город']['НаимГород']}, {addr['Улица']['ТипУлица']} 
-        {addr['Улица']['НаимУлица']}, {addr['Дом'] + subaddr}, {addr['Индекс']}"""
-    return result
-
-
-def analytics(file_data):
-    """checks the parameters of the json data for compliance with the search, 
-    if successful, adds the data to the database"""
+    def __init__(self) -> None:
+        self.operations = Operations()
     
-    for unit in file_data:
-        data = unit["data"]
-        code = None
+    @staticmethod
+    def archive_unpacker(part_length: int) -> json:
+        """unpacks the archive and alternately gives the contents of the files to json"""
 
-        if data.get("СвОКВЭД") and data["СвОКВЭД"].get("СвОКВЭДОсн"):
-            if data["СвОКВЭД"]["СвОКВЭДОсн"]["КодОКВЭД"] == settings.CODE:
-                code = data["СвОКВЭД"]["СвОКВЭДОсн"]["КодОКВЭД"]
+        with ZipFile(f"../{settings.ARCHIVE}", mode="r") as archive:
+            filenames = [filename for filename in archive.namelist()]
 
-        if code: 
-            if data["СвАдресЮЛ"].get("АдресРФ") and data["СвАдресЮЛ"]["АдресРФ"].get("Город"):
-                if data["СвАдресЮЛ"]["АдресРФ"]["Город"]["НаимГород"] == settings.NAME_CITY:
-                    city = data["СвАдресЮЛ"]["АдресРФ"]
-                    correct_address = address_handler(city)
-                    result = {
-                        "name": unit["full_name"],
-                        "code": code,
-                        "inn": int(unit["inn"]),
-                        "kpp": int(unit["kpp"]),
-                        "address": correct_address
-                    }
-                    Operations.add_company(result)
+            partition = (filenames[i:i + part_length] for i in range(0, len(filenames), part_length))
+
+            for part in list(partition):
+                archive.extractall(members=part)
+                for filename in part:
+                    with open(filename, encoding='utf-8') as file_json:
+                        logger.info(f"file {filename} unpacked and open")
+
+                        file_data = json.load(file_json)
+                        yield file_data
+
+                    os.remove(filename)
+                    logger.info(f"file {filename} removed")
+
+    @staticmethod
+    def address_handler(addr: json) -> str:
+        """converts address data to a string"""
+
+        if addr.get('Корпус'):
+            subaddr = ', ' + addr['Корпус']
+        elif addr.get('Кварт'):
+            subaddr = ', ' + addr['Кварт']
+        else:
+            subaddr = ''
+
+        result = f"""{addr['Регион']['НаимРегион']} {addr['Регион']['ТипРегион']}, 
+            {addr['Город']['ТипГород']} {addr['Город']['НаимГород']}, {addr['Улица']['ТипУлица']} 
+            {addr['Улица']['НаимУлица']}, {addr['Дом'] + subaddr}, {addr['Индекс']}"""
+        return result
+
+    def analytics(self, file_data: json) -> None:
+        """checks the parameters of the json data for compliance with the search, 
+        if successful, adds the data to the database"""
+        
+        for unit in file_data:
+            data = unit["data"]
+            code = None
+
+            if data.get("СвОКВЭД") and data["СвОКВЭД"].get("СвОКВЭДОсн"):
+                if data["СвОКВЭД"]["СвОКВЭДОсн"]["КодОКВЭД"] == settings.CODE:
+                    code = data["СвОКВЭД"]["СвОКВЭДОсн"]["КодОКВЭД"]
+
+            if code: 
+                if data["СвАдресЮЛ"].get("АдресРФ") and data["СвАдресЮЛ"]["АдресРФ"].get("Город"):
+                    if data["СвАдресЮЛ"]["АдресРФ"]["Город"]["НаимГород"] == settings.NAME_CITY:
+                        city = data["СвАдресЮЛ"]["АдресРФ"]
+                        correct_address = self.address_handler(city)
+                        result = {
+                            "name": unit["full_name"],
+                            "code": code,
+                            "inn": int(unit["inn"]),
+                            "kpp": int(unit["kpp"]),
+                            "address": correct_address
+                        }
+                        self.operations.add_company(result)
+
+
+def core() -> None:
+    """the core of project"""
     
-
-def core():
+    main = Main()
     try:
-        au = archive_unpacker(settings.UNPACK_FILES_COUNT)
+        au = main.archive_unpacker(settings.UNPACK_FILES_COUNT)
         while True:
             json_data = next(au)
-            analytics(json_data)
+            main.analytics(json_data)
     except StopIteration:
         return
 
